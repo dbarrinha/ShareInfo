87c712353a0d63fe37df899e1dc6a43b0d1d0d63
Computacao2014 <cc2014.1@outlook.com>
2017-11-23 15:25:47 -0300

    dinamico

---
+++ controller/questao.php
@@ -0,0 +1,21 @@
+<?php
+
+$tipo_pergunta = $_GET['tipoPergunta'];
+$num = 3;
+#tipo 1 = resposta curta
+if($tipo_pergunta == 1){
+   for($i= 0; $i < $num; $i++){
+        echo "<input type='radio' name='op' value='{$i}'> opção ".$i;
+    }
+}else if($tipo_pergunta == 2){
+    for($i= 0; $i < $num; $i++){
+        echo "<input type='checkbox' name='op{$i}'> opção ".$i;
+    }
+    
+}else if($tipo_pergunta == 3){
+    
+     echo "<input type='text' name='texto'>";
+}else{
+    echo "";
+}
+
