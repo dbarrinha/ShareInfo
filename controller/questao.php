afdc6c32def78cfabf90d7d8decf6e1a70b407a8
Computacao2014 <cc2014.1@outlook.com>
2017-11-23 22:45:59 -0300

    numero de respostas

--- controller/questao.php
+++ controller/questao.php
@@ -1,7 +1,7 @@
 <?php
 
 $tipo_pergunta = $_GET['tipoPergunta'];
-$num = 3;
+$num = $_GET['num'];
 #tipo 1 = resposta curta
 if($tipo_pergunta == 1){
    for($i= 0; $i < $num; $i++){
@@ -11,9 +11,7 @@ if($tipo_pergunta == 1){
     for($i= 0; $i < $num; $i++){
         echo "<input type='checkbox' name='op{$i}'> opção ".$i;
     }
-    
 }else if($tipo_pergunta == 3){
-    
      echo "<input type='text' name='texto'>";
 }else{
     echo "";
