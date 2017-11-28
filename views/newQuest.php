6cbb00a950849e665e55310b73b18bc37e8ac0ba
danilo.barrinha <danilo.barrinha@DISAU-07122.tce.pi.gov.br>
2017-11-28 11:04:32 -0300

    pergunta

--- views/newQuest.php
+++ views/newQuest.php
@@ -1,15 +1,16 @@
 <?php 
-
+//$id_pergunta = $_GET["id_pergunta"];
+$id= 2;
 $pergunta = "<hr/>
-    <div id='pergunta3'>
-                        <input class='form-control' type='text' id='pergunta' placeholder='Pergunta' >
-                        <select  id='menu' class='form-control bg-3'   onchange='escolheResposta()'>
+    <div id='{$id}'>
+                        <input class='form-control' type='text' id='titulo{$id}' placeholder='Pergunta' >
+                        <select  id='menu{$id}' class='form-control bg-3'   onchange='escolheResposta()'>
                             <option value=' ' selected='selected'>Escolha a forma de resposta</option>
                             <option value='1' >Única Opção</option>
                             <option value='2'>Multipla Escolha</option>
                             <option value='3'>Discursiva</option>
                         </select>
-                        <select  id='num' class='form-control bg-3' style='display:none'  onchange='escolheRespostaNum()'>
+                        <select  id='num{$id}' class='form-control bg-3' style='display:none'  onchange='escolheRespostaNum()'>
                             <option value='0' selected='selected' > Escolha o numero de respostas</option>
                             <option value='1' >1</option>
                             <option value='2'>2</option>
@@ -19,7 +20,7 @@ $pergunta = "<hr/>
                             <option value='6'>6</option>
                             <option value='7'>7</option>
                         </select>
-                        <div id='pergunta2'>
+                        <div id='pergunta{$id}'>
                         </div> 
                     </div>";
 
